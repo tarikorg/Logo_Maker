e1031b503b55cfbafd8994dddc1e6b7140801bba
tarikorg <myku1771@gmail.com>
2024-04-14 17:28:56 -0400

    commit classes for shapes created, syntax changed, example folder completed, tests are functional

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
-const Shape = require('./lib/shapes')
+const { Circle, Square, Triangle } = require('./lib/shapes')
 
 
 inquirer.prompt(
-[
-{
-    type: 'list',
-    name: 'shape',
-    message: 'Select shape',
-    choices: ['circle', 'square','triangle']
-},
-{
-    type: 'input',
-    name: 'text',
-    message: 'Enter text (up to 3 characters):',
-    // max 3 char
-    validate: function (value){
-        if (value.length > 3){
-            return 'Please enter up to 3 characters'
+    [
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select shape',
+            choices: ['circle', 'square', 'triangle']
+        },
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter text (up to 3 characters):',
+            // max 3 char
+            validate: function (value) {
+                if (value.length > 3) {
+                    return 'Please enter up to 3 characters'
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color (keyword or hexadecimal number example: #1231231):'
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter shape color (keyword or hexadecimal number example: #123123):'
         }
-        return true;
+
+
+    ]
+).then((userInput) => {
+    let makeShape
+    switch (userInput.shape) {
+        case 'circle':
+            makeShape = new Circle(userInput.text, userInput.textColor, userInput.shapeColor)
+            break
+        case 'square':
+            makeShape = new Square(userInput.text, userInput.textColor, userInput.shapeColor)
+            break
+        case 'triangle':
+            makeShape = new Triangle(userInput.text, userInput.textColor, userInput.shapeColor)
+            break
     }
-},
-{
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter text color (keyword or hexadecimal number example: #1231231):'
-},
-{
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter shape color (keyword or hexadecimal number example: #123123):'
-}
-
-
-]
-).then((userInput)=>{
-    const makeShape = new Shape(
-        userInput.shape,
-        userInput.text,
-        userInput.textColor,
-        userInput.shapeColor
-    )
 
     const svg = makeShape.createSVG()
 
-    fs.writeFile('./createdSvg/logo.svg', svg , function (err){
-        if(err){
-            console.log('error: ',err)
-        }else{
+    fs.writeFile('./createdSvg/logo.svg', svg, function (err) {
+        if (err) {
+            console.log('error: ', err)
+        } else {
             console.log('Generated logo.svg')
         }
     })
